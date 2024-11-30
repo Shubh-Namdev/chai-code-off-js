0aa0b48607de40d768c469efbd2f3f5222f3c80a
Shubham Namdev <s.namdev013@gmail.com>
2024-11-30 14:53:46 +0530

    type revision added

---
+++ lecture/revTypes.js
@@ -0,0 +1,18 @@
+//Primitve
+
+let number = 23;
+let text = "text";
+const isHuman = true;
+const profession = null;
+let age;
+let id = Symbol("34");
+let eId = Symbol("34");
+
+let arr = ["mangor","grapes","apple"];
+let myObj = {
+    name : "Adam",
+    address:{city:"London",state:"UK"},
+    willOperate:function() { }
+}
+
+console.log(typeof myObj)
